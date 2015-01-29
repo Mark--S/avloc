0d29ae0369b9c93703f78fca6048933a93753cc9
Mark Stringer <ms711@feynman.cm.cluster>
2015-01-29 14:05:51 +0000

    Edited runMakeNtuple to run on batch

--- runMakeNtuple.sh
+++ runMakeNtuple.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-#source /mnt/lustre/epp_scratch/neutrino/rat/snoing/install/env_rat-5.0.0.sh
 for file in outputs/Fibre_FT0* 
 do
-    echo $file
-    bin/make_ntuple $file
-
+    qsub -cwd -q mps.q ./submit.sh $file 
 done
