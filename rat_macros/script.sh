6d9bb46b290b4d6176eda8aa1129d8e85b176919
Mark Stringer <ms711@feynman.cm.cluster>
2015-02-16 10:10:38 +0000

    Fixed submission scripts

--- rat_macros/script.sh
+++ rat_macros/script.sh
@@ -1,7 +1,6 @@
 #$$ -S /bin/bash
-#$$ -o /epp/scratch/neutrino/sjp39/logs
-#$$ -e /epp/scratch/neutrino/sjp39/logs
+#$$ -o /epp/scratch/neutrino/ms711/logs
+#$$ -e /epp/scratch/neutrino/ms711/logs
 #$$ -q mps.q
-cd /epp/scratch/neutrino/sjp39
-source env_rat-dev.sh
+source /mnt/lustre/epp_scratch/neutrino/rat/snoing/install/env_rat-5.0.0.sh
 rat -l ${Log} -o ${Output} ${Macro}
